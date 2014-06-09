a295069e8818b9c68547a9fd77bf634639c1d3a8
Victor <victor.miletskiy@gmail.com>
2014-06-09 16:13:31 +0300

    Moved to dir

---
+++ ukrainian/db_lang.php
@@ -0,0 +1,30 @@
+<?php
+/* https://github.com/miletskiy/codeigniter-ukrainian.git */
+
+$lang['db_invalid_connection_str']		= 'Неможливо визначити параметри базы даних на основы наданого рядка DSN.'; // looks odd :|
+$lang['db_unable_to_connect']			= 'Неможливо підключитись до серверу бази даних використовуючи надані параметри.';
+$lang['db_unable_to_select']			= 'Неможливо вибрати вказану базу даних: %s';
+$lang['db_unable_to_create']			= 'Неможливо створити вказану базу даних: %s';
+$lang['db_invalid_query']				= 'Представлений sql-запит є недопустимим.';
+$lang['db_must_set_table']				= 'Необхідно вказати таблицю бази даних, що буде використовуватись у запиті.';
+$lang['db_must_use_set']				= 'Необхідно використовувати метод "set" для оновлення записів.';
+$lang['db_must_use_index']				= 'Необхідно вказати індекс для пакетного оновлення.';
+$lang['db_batch_missing_index']			= 'Один або більше рядків, представлених для пакетного оновлення, не містять вказаний індекс.';
+$lang['db_must_use_where']				= 'Неможливо оновити рядки без вказання фільтру через "where" оператор.';
+$lang['db_del_must_use_where']			= 'Неможливо видалити рядки без вказання фільтру через "where" або "like" оператор.';
+$lang['db_field_param_missing']			= 'Для вибірки необхідно вказати назву таблиці як параметр.';
+$lang['db_unsupported_function']		= 'Ця функція не підтримується базою даних, що ви використовуєте.';
+$lang['db_transaction_failure']			= 'Помилка транзакції: виконується відкат.';
+$lang['db_unable_to_drop']				= 'Неможливо видалити вказану базу даних.';
+$lang['db_unsuported_feature']			= 'Ця функція не підтримується базою даних, що ви використовуєте.';
+$lang['db_unsuported_compression']		= 'Вказазаний формат стискання данних не підтримується вашим сервером.';
+$lang['db_filepath_error']				= 'Неможливо записати дані в файл використовуючи вказану директорію.';
+$lang['db_invalid_cache_path']			= 'Директорія з кеш файлами вказана некоректно або недоступна для запису.';
+$lang['db_table_name_required']			= 'Для цієї операції необхідно вказати назву таблиці.';
+$lang['db_column_name_required']		= 'Для цієї операції необхідно вказати назву колонки.';
+$lang['db_column_definition_required']	= 'Для цієї оперції необхидний опис колонки (column definition).';
+$lang['db_unable_to_set_charset']		= 'Неможливо встановити кодування з&#39;єднання: %s';
+$lang['db_error_heading']				= 'Помилка бази даних.';
+
+/* End of file db_lang.php */
+/* Location: ./system/language/ukrainian/db_lang.php */
